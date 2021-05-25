47d7747cd5a6505017bd79ff1e1b7f77914ab14a
Dawidx3p <maildawcio@gmail.com>
2021-05-25 23:41:02 +0200

    new projects added

--- src/components/MyProjects/MyProjects.js
+++ src/components/MyProjects/MyProjects.js
@@ -7,9 +7,11 @@ export default function MyProjects() {
             <Col>
                 <h2>My projects</h2>
                 <ul>
-                <li><a target="_blank" rel="noopener noreferrer external" href="https://spotthelist.tk">SpotTheList</a></li>
+                <li><a target="_blank" rel="noopener noreferrer external" href="https://spotthelist.tk">SpotTheList (React.js)</a></li>
                 <li><a target="_blank" rel="noopener noreferrer external" href="https://github.com/Dawidx3p/Expresso">Expresso</a></li>
-                <li><a target="_blank" rel="noopener noreferrer external" href="https://github.com/Dawidx3p/bossMachine">Boss Machine</a></li></ul>
+                <li><a target="_blank" rel="noopener noreferrer external" href="https://github.com/Dawidx3p/bossMachine">Boss Machine</a></li>
+                <li><a target="_blank" rel="noopener noreferrer external" href="https://orangehomepage.tk">Orange Homepage (HTML/CSS)</a></li>
+                <li><a target="_blank" rel="noopener noreferrer external" href="https://flamboyant-goldberg-bbefb7.netlify.app">Chess Club Homepage (HTML/CSS)</a></li></ul>
                 
             </Col>
         </Row>
